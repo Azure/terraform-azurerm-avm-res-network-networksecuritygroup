57f032f243acb3bac5687b1ead108b44a53ce7fb
PJ <prjelesi@microsoft.com>
2024-02-12 13:37:48 +0100

    w1

--- main.tf
+++ main.tf
@@ -1,1 +1,74 @@
 # TODO: insert resources here.
+
+
+resource "azurerm_network_security_group" "this" {
+  name                    = var.name
+  resource_group_name     = var.resource_group_name
+  location                = var.location
+
+#   security_rule {
+#     name                       = "test123"
+#     priority                   = 100
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "*"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+
+  
+}
+
+resource "azurerm_management_lock" "this" {
+  count      = var.lock.kind != "None" ? 1 : 0
+  name       = coalesce(var.lock.name, "lock-${var.name}")
+  scope      = azurerm_public_ip.this.id
+  lock_level = var.lock.kind
+}
+
+resource "azurerm_role_assignment" "this" {
+  for_each                               = var.role_assignments
+  scope                                  = azurerm_public_ip.this.id
+  role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
+  role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
+  principal_id                           = each.value.principal_id
+  condition                              = each.value.condition
+  condition_version                      = each.value.condition_version
+  skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
+  delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "this" {
+  for_each                       = var.diagnostic_settings
+  name                           = each.value.name != null ? each.value.name : "diag-${var.name}"
+  target_resource_id             = azurerm_public_ip.this.id
+  storage_account_id             = each.value.storage_account_resource_id
+  eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
+  eventhub_name                  = each.value.event_hub_name
+  partner_solution_id            = each.value.marketplace_partner_resource_id
+  log_analytics_workspace_id     = each.value.workspace_resource_id
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+
+  dynamic "enabled_log" {
+    for_each = each.value.log_categories
+    content {
+      category = enabled_log.value
+    }
+  }
+
+  dynamic "enabled_log" {
+    for_each = each.value.log_groups
+    content {
+      category_group = enabled_log.value
+    }
+  }
+
+  dynamic "metric" {
+    for_each = each.value.metric_categories
+    content {
+      category = metric.value
+    }
+  }
+}
