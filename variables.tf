e83e6d4262c09e10b5105336253e38e293cb4933
PJ <prjelesi@microsoft.com>
2024-02-13 12:57:30 +0100

    rule_variable_update

--- variables.tf
+++ variables.tf
@@ -34,15 +34,15 @@ variable "location" {
 variable "nsgrules" {
   type = map(object(
     {
-      nsg_rule_name                       = list(string) # (Required) Name of NSG rule.
-      nsg_rule_priority                   = list(string) # (Required) NSG rule priority.
-      nsg_rule_direction                  = list(string) # (Required) NSG rule direction. Possible values are `Inbound` and `Outbound`.
-      nsg_rule_access                     = list(string) # (Required) NSG rule access. Possible values are `Allow` and `Deny`.
-      nsg_rule_protocol                   = list(string) # (Required) NSG rule protocol. Possible values are `Tcp`, `Udp`, `Icmp`, `Esp`, `Asterisk`.
-      nsg_rule_source_port_range          = list(string) # (Required) NSG rule source port range.
-      nsg_rule_destination_port_range     = list(string) # (Required) NSG rule destination port range.
-      nsg_rule_source_address_prefix      = list(string) # (Required) NSG rule source address prefix.
-      nsg_rule_destination_address_prefix = list(string) # (Required) NSG rule destination address prefix.
+      nsg_rule_name                       = string # (Required) Name of NSG rule.
+      nsg_rule_priority                   = number # (Required) NSG rule priority.
+      nsg_rule_direction                  = string # (Required) NSG rule direction. Possible values are `Inbound` and `Outbound`.
+      nsg_rule_access                     = string # (Required) NSG rule access. Possible values are `Allow` and `Deny`.
+      nsg_rule_protocol                   = string # (Required) NSG rule protocol. Possible values are `Tcp`, `Udp`, `Icmp`, `Esp`, `Asterisk`.
+      nsg_rule_source_port_range          = string # (Required) NSG rule source port range.
+      nsg_rule_destination_port_range     = string # (Required) NSG rule destination port range.
+      nsg_rule_source_address_prefix      = string # (Required) NSG rule source address prefix.
+      nsg_rule_destination_address_prefix = string # (Required) NSG rule destination address prefix.
     }
   ))
   description = "NSG rules to create"
