a4e05a72c400893b308891ff9a10cb4ad37fd602
PJ <prjelesi@microsoft.com>
2024-02-13 15:48:17 +0100

    example update

--- examples/default/README.md
+++ examples/default/README.md
@@ -54,13 +54,7 @@ The following resources are used by this module:
 <!-- markdownlint-disable MD013 -->
 ## Required Inputs
 
-The following input variables are required:
-
-### <a name="input_rules"></a> [rules](#input\_rules)
-
-Description: Variable to define the NSG rules to be created.
-
-Type: `any`
+No required inputs.
 
 ## Optional Inputs
 
@@ -93,6 +87,45 @@ The default value is "eastus".
 Type: `string`
 
 Default: `"eastus"`
+
+### <a name="input_rules"></a> [rules](#input\_rules)
+
+Description: NSG rules to create
+
+Type:
+
+```hcl
+map(object(
+    {
+      # nsg_rule_name                       = string # (Required) Name of NSG rule.
+      nsg_rule_priority                   = number # (Required) NSG rule priority.
+      nsg_rule_direction                  = string # (Required) NSG rule direction. Possible values are `Inbound` and `Outbound`.
+      nsg_rule_access                     = string # (Required) NSG rule access. Possible values are `Allow` and `Deny`.
+      nsg_rule_protocol                   = string # (Required) NSG rule protocol. Possible values are `Tcp`, `Udp`, `Icmp`, `Esp`, `Asterisk`.
+      nsg_rule_source_port_range          = string # (Required) NSG rule source port range.
+      nsg_rule_destination_port_range     = string # (Required) NSG rule destination port range.
+      nsg_rule_source_address_prefix      = string # (Required) NSG rule source address prefix.
+      nsg_rule_destination_address_prefix = string # (Required) NSG rule destination address prefix.
+    }
+  ))
+```
+
+Default:
+
+```json
+{
+  "rule01": {
+    "nsg_rule_access": "Allow",
+    "nsg_rule_destination_address_prefix": "*",
+    "nsg_rule_destination_port_range": "*",
+    "nsg_rule_direction": "Inbound",
+    "nsg_rule_priority": 100,
+    "nsg_rule_protocol": "Tcp",
+    "nsg_rule_source_address_prefix": "*",
+    "nsg_rule_source_port_range": "*"
+  }
+}
+```
 
 ## Outputs
 
--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -27,18 +27,32 @@ DESCRIPTION
 }
 
 variable "rules" {
-  rule1 = {
-    nsg_rule_priority                   = 100
-    nsg_rule_direction                  = "Outbound"
-    nsg_rule_access                     = "Allow"
-    nsg_rule_protocol                   = "Tcp"
-    nsg_rule_source_port_range          = "*"
-    nsg_rule_destination_port_range     = "*"
-    nsg_rule_source_address_prefix      = "*"
-    nsg_rule_destination_address_prefix = "*"
+  type = map(object(
+    {
+      # nsg_rule_name                       = string # (Required) Name of NSG rule.
+      nsg_rule_priority                   = number # (Required) NSG rule priority.
+      nsg_rule_direction                  = string # (Required) NSG rule direction. Possible values are `Inbound` and `Outbound`.
+      nsg_rule_access                     = string # (Required) NSG rule access. Possible values are `Allow` and `Deny`.
+      nsg_rule_protocol                   = string # (Required) NSG rule protocol. Possible values are `Tcp`, `Udp`, `Icmp`, `Esp`, `Asterisk`.
+      nsg_rule_source_port_range          = string # (Required) NSG rule source port range.
+      nsg_rule_destination_port_range     = string # (Required) NSG rule destination port range.
+      nsg_rule_source_address_prefix      = string # (Required) NSG rule source address prefix.
+      nsg_rule_destination_address_prefix = string # (Required) NSG rule destination address prefix.
+    }
+  ))
+  default = {
+    "rule01" = {
+      nsg_rule_access                     = "Allow"
+      nsg_rule_destination_address_prefix = "*"
+      nsg_rule_destination_port_range     = "*"
+      nsg_rule_direction                  = "Inbound"
+      nsg_rule_priority                   = 100
+      nsg_rule_protocol                   = "Tcp"
+      nsg_rule_source_address_prefix      = "*"
+      nsg_rule_source_port_range          = "*"
+
+    }
   }
-  description = <<DESCRIPTION
-Variable to define the NSG rules to be created.
-DESCRIPTION
+  description = "NSG rules to create"
 }
 
