0d36912256437e2391c1210b1b19f3b23928bad8
PJ <prjelesi@microsoft.com>
2024-02-14 10:31:55 +0100

    updated based on comment

--- main.tf
+++ main.tf
@@ -5,20 +5,7 @@ resource "azurerm_network_security_group" "this" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
-
-  #   security_rule {
-  #     name                       = "test123"
-  #     priority                   = 100
-  #     direction                  = "Inbound"
-  #     access                     = "Allow"
-  #     protocol                   = "Tcp"
-  #     source_port_range          = "*"
-  #     destination_port_range     = "*"
-  #     source_address_prefix      = "*"
-  #     destination_address_prefix = "*"
-  #   }
-
-  tags = var.tags
+  tags                = var.tags
 
 }
 
