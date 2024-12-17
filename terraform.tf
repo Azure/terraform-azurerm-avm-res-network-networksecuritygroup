3d85afb2629c42f755acc0fd0ed395ee7efa196d
Tobias Lindberg <tobias.ehlert@gmail.com>
2024-12-17 10:04:12 +0100

    fix: add Azure/modtm to providers

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.71, < 5.0"
     }
+    modtm = {
+      source  = "Azure/modtm"
+      version = "~> 0.3"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
