e786c7d893a60e199d57a0451db9f4d7b6cc6432
Rhys Ash <rhysash@microsoft.com>
2026-01-15 12:10:08 +0000

    fix: update resource group location to a static value and adjust NSG rule attributes for idempotency

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,15 @@ output "resource_id" {
 }
 
 output "security_rules" {
-  description = "The Network Security Group Rules"
-  value       = azurerm_network_security_rule.this
+  description = "The Network Security Group Rules" # Adjusting certain attributes for idempotent plans
+  value = {
+    for key, rule in azurerm_network_security_rule.this : key => merge(rule, {
+      destination_address_prefixes               = rule.destination_address_prefixes == null ? [] : rule.destination_address_prefixes
+      destination_application_security_group_ids = rule.destination_application_security_group_ids == null ? [] : rule.destination_application_security_group_ids
+      destination_port_ranges                    = rule.destination_port_ranges == null ? [] : rule.destination_port_ranges
+      source_address_prefixes                    = rule.source_address_prefixes == null ? [] : rule.source_address_prefixes
+      source_application_security_group_ids      = rule.source_application_security_group_ids == null ? [] : rule.source_application_security_group_ids
+      source_port_ranges                         = rule.source_port_ranges == null ? [] : rule.source_port_ranges
+    })
+  }
 }
